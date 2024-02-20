74515f5e38c694baf5a2ea772271b6db1aa6475e
raJiB <pokhrelrajib016@gmail.com>
2024-02-20 15:23:30 +0545

    db connections

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+const dbConnection = async function () {
+    try {
+        const response = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        if(response){
+            console.log("DB connected successfully");
+        }
+
+    } catch (error) {
+        console.log("Error in db connnection", error);
+        process.exit(1);
+    }
+}
+
+export default dbConnection;
